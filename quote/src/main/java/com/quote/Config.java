caa82df5fb5a4a5b0af679302cd9cb86d1cde037
Soumyadeep <Soumyadeep@bitbucket.org>
2016-02-13 23:55:09 +1100

    updated

--- quote/src/main/java/com/quote/Config.java
+++ quote/src/main/java/com/quote/Config.java
@@ -3,8 +3,6 @@ package com.quote;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
